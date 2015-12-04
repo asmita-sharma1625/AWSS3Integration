797f47b815af8aab4b8a1874f435466c18c2c159
asmita-sharma1625 <asmita_sharma@outlook.com>
2015-12-04 15:18:12 +0530

    adding mailClient and change applier classes. Change detector now also constructs diff files

--- configChangeNotifier.py
+++ configChangeNotifier.py
@@ -1,56 +1,64 @@
 from s3Dao import S3Dao
-import logging
 from configReader import ConfigReader
-import os
-from helper import Helper
-
-logger = logging.getLogger("s3Integration")
-
-class ConfigChangeNotifier:
-  
-  def __init__(self, bucket, objectKey):
-    self.dao = S3Dao()
-    self.dao.setBucket(bucket)
-    self.objectKey = objectKey
-    self.oldConfig = Helper.getConfigFromRemote(self.objectKey)
-
-  def NotifyIfConfigChange(self):
-    newConfig = "/tmp/newConfig.conf"
-    configChangeDetector = ConfigChangeDetector(self.oldConfig, newConfig)
-    open(newConfig, "w").close()
-    self.dao.downloadObject(self.objectKey, newConfig)
-    if configChangeDetector.compareConfig():
-      #self.dao.uploadObject(pathname, objectKey)    
-    os.remove(newConfig)
 
 class ConfigChangeDetector:
-
+  
+  '''
+    detects changes in old and new config. Also creates diff files for new and old configs. 
+  '''
+  
   def __init__(self, oldConfig, newConfig):
-    self.oldConfig = oldConfig
     self.newConfig = newConfig
+    self.oldConfig = oldConfig
     self.oldConfigReader = ConfigReader(self.oldConfig)
     self.newConfigReader = ConfigReader(self.newConfig)
-    self.flag = False
-
+    self.diff_flag = False
+    self.diff_config_old = "/tmp/config_diff_old.conf"
+    self.diff_old_config_reader = ConfigReader(self.diff_config_old)
+    self.diff_config_new = "/tmp/config_diff_new.conf"
+    self.diff_new_config_reader = ConfigReader(self.diff_config_new)
+    
   def compareConfig(self):
     oldSections = self.oldConfigReader.getSections()
     newSections = self.newConfigReader.getSections()
     for section in newSections:
       if section not in oldSections:
-        oldConfigReader.addSection(section)
-        self.flag = True  
-      Helper.compareSection(oldConfig, newConfig, section)
-    return self.flag
+        self.diff_new_config_reader.addSection(section)
+        self.diff_flag = True  
+      self.compareSection(oldConfig, newConfig, section)
+    return self.diff_flag
 
   def compareSection(self, section):
     oldKeys = self.oldConfigReader.getKeys(section)
     newKeys = self.newConfigReader.getKeys(section)
     for key in newKeys:
       if key not in oldKeys:
-        self.flag = True
-        # Trigger Notification
-        #oldConfigReader.setValue(section, key, newConfigReader.getValue(section, key))
+        self.diff_new_config_reader.setValue(section, key, self.diff_new_config_reader.getValue(section, key)
+        self.diff_flag = True
       oldValue = self.oldConfigReader.getValue(section, key)
       newValue = self.newConfigReader.getValue(section, key)
       if oldValue != newValue:
-        self.flag = True
+        self.diff_old_config_reader.setValue(section, key, self.diff_old_config_reader.getValue(section, key)
+        self.diff_new_config_reader.setValue(section, key, self.diff_new_config_reader.getValue(section, key)
+        self.diff_flag = True
+    return self.diff_flag
+
+  def getDiffInOldConfig(self):
+    return self.diff_config_old
+
+  def getDiffInNewConfig(self):
+    return self.diff_config_new
+
+
+
+class ConfigChangeApplier:
+  
+  '''
+    applies config change to remote host by copying new config to the specified path on the given node.  
+  '''
+
+  def __init__(self, config, node, path):
+    self.config = config
+    self.node = node
+    self.path = path 
+    Helper.copyConfigToRemote(config, node, path) 
--- notificationMailMonitor.py
+++ notificationMailMonitor.py
@@ -1,30 +1,82 @@
-import poplib
-from email import parser
-import sys
+from configChangeNotifier import ConfigChangeNotifier
 
-host = 'pop.mail.yahoo.com'
-user = 's3.notifier@yahoo.com'
-passwd = 'jio@1234'
+class S3NotificationMonitor:
 
-pop_conn = poplib.POP3_SSL(host)
-print pop_conn.getwelcome()
-try:
-  print "authenticating..."
-  pop_conn.user(user)
-  pop_conn.pass_(passwd)
-except poplib.error_proto, e:
-  print "Login failed:", e
-  sys.exit(1)
+  '''
+  Sample S3 Notification Mail Body:
+        {"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"us-west-2","eventTime":"2015-12-03T11:47:40.428Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"AWS:AIDAI3LQQTO4RURMDMETK"},"requestParameters":{"sourceIPAddress":"49.32.0.187"},"responseElements":{"x-amz-request-id":"EBA9BB89874EF559","x-amz-id-2":"C+xu0zv/OxCgO22mZ2PtN5ubNw5+doOW9vJnUIuyWszdt6/I3c4HboOPLQnUM4rz"},"s3":{"s3SchemaVersion":"1.0","configurationId":"MyEmailNotificationsForPut","bucket":{"name":"compute-config","ownerIdentity":{"principalId":"A2GX7JMHUJDOBN"},"arn":"arn:aws:s3:::compute-config"},"object":{"key":"subfolder/Compute-Read","size":106,"eTag":"1caf53f68ac7f0a4a84bd6b163d0a564","versionId":"63P9JdVgXKXJRLe235EeCqJZdrASwGdZ","sequencer":"0056602BDC5B063096"}}}]}
+  '''
 
-print "retrieving messages..."
-#Get messages from server:
-messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
-# Concat message pieces:
-messages = ["\n".join(mssg[1]) for mssg in messages]
-#Parse message intom an email object:
-messages = [parser.Parser().parsestr(mssg) for mssg in messages]
-for message in messages:
-  print "message subject :: ", message['subject']
+  def __init__(self):
+    server = 'pop.mail.yahoo.com'
+    user = 's3.notifier@yahoo.com'
+    passwd = 'jio@1234'
+    self.mailClient = MailClient(server, user, passwd)
+    self.S3_BODY_KEY_1_LEVEL_1 = "Records"
+    self.S3_BODY_KEY_1_LEVEL_2 = "s3"
+    self.S3_BODY_KEY_1_LEVEL_3 = "object"
+    self.S3_BODY_KEY_1_LEVEL_4 = "key"
+    self.S3_BODY_KEY_2_LEVEL_4 = "versionId"
+    self.S3_BUCKET = "compute-config"
+    self.s3Dao = S3Dao()
+    self.s3Dao.setBucket(self.S3_BUCKET)
 
-pop_conn.quit()
+    s3NotifyMsg = self.getS3NotificationMailBody()
+    if s3NotifyMsg is not None:
+      objectKey = self.getS3ObjectKey(s3NotifyMsg)
+      objectVersion = self.getS3ObjectVersion(s3NotifyMsg)
+      newConfig = "/tmp/newConfig.conf"
+      self.getS3Object(objectKey, NEW_CONFIG_PATH)
+      OLD_CONFIG_PATH = self.getConfigPath(objectKey)
+      OLD_CONFIG_NODE = self.getConfigNode(objectKey)
+      oldConfig = "/tmp/oldConfig.conf"
+      self.getConfig(OLD_CONFIG_PATH, OLD_CONFIG_NODE, oldConfig)
+      ''' notify any change to registered user '''
+      self.notifyChange(oldConfig, newConfig)
+
+    self.responseMailClient = MailClient(server, user, passwd) 
+    self.applyChange(newConfig, OLD_CONFIG_PATH, OLD_CONFIG_NODE)
+
+  def getS3NotificationMail(self):
+    return self.mailClient.getMail(deleteRead = True, section = 'subject', regex = 'conf_put*')
+  
+  def getS3NotificationMailBody(self):
+    mail = self.getS3NotificationMail()
+    if mail is not None:
+      return self.mailClient.getMailBody(mail)
+    return None
+
+  def getS3ObjectKey(self, message):
+    return message[self.S3_BODY_KEY_1_LEVEL_1][self.S3_BODY_KEY_1_LEVEL_2][self.S3_BODY_KEY_1_LEVEL_3][self.S3_BODY_KEY_1_LEVEL_4]
+
+  def getS3ObjectVersion(self, message):
+    return message[self.S3_BODY_KEY_1_LEVEL_1][self.S3_BODY_KEY_1_LEVEL_2][self.S3_BODY_KEY_1_LEVEL_3][self.S3_BODY_KEY_2_LEVEL_4]
+    
+  def getS3Object(self, objectKey, newConfig):
+    self.s3Dao.downloadObject(objectKey, newConfig)
+
+  def getConfigPath(self, objectKey):
+    return Helper.mapToPath(objectKey)
+
+  def getConfigNode(self, objectKey):
+    return Helper.mapToNode(objectKey)
+
+  def getConfig(self, node, src_path, dest_path = None):
+    return Helper.copyConfigFromRemote(node, src_path, dest_path)
+
+  def notifyChange(self, oldConfig, newConfig):
+    configChangeDetector = ConfigChangeDetector(oldConfig, newConfig)
+    if configChangeDetector.compareConfig():
+      old_config_diff = configChangeDetector.getDiffInOldConfig()
+      new_config_diff = configChangeDetector.getDiffInNewConfig()
+      #call mail server to send notification with diff 
+    
+  def applyChange(self, newConfig, node, path):
+    if self.getResponseMail() is not None:
+      ConfigChangeApplier(newConfig, node, path).copyConfigToRemote()
+
+  def getResponseMail(self):
+    return self.responseMailClient.getMail(deleteRead = True, section = 'subject', regex = '*conf_change_apply*')
+
+
 
